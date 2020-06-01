333310c5d7c16b448d04358953ea527cbe4f7290
rhiny-build <ronen.elman@gmail.com>
2020-06-01 20:38:31 +0100

    backend complete

---
+++ server.js
@@ -0,0 +1,17 @@
+import config from './config';
+import apiRouter from './api'
+import express from 'express';
+
+const server = express();
+server.set('view engine', 'ejs');
+
+server.get ('/', (req,res) => {
+    res.render('index', {content: 'woot <em>woot</em> '});
+});
+server.use('/api', apiRouter);
+
+server.use(express.static('public'));
+
+server.listen (config.port, () => {
+    console.info('all good');
+});
